f41858cbbba5d1b8842d7d542e6a7f09fca31ded
SinchanaGanapatiNaik <nsinchana506@gmail.com>
2026-02-23 18:24:03 +0530

    Create admin dashboard page

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+function AdminDashboard() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold">
+        Welcome to Admin Dashboard 🛕
+      </h1>
+    </div>
+  )
+}
+
+export default AdminDashboard
